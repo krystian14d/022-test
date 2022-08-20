533c353f0cafe46af9363b35c3b1992e3c9cf639
Krystian <krystian14d@gmail.com>
2022-08-20 09:48:00 +0200

    Solution

--- src/main/java/com/devskiller/dao/ItemDao.java
+++ src/main/java/com/devskiller/dao/ItemDao.java
@@ -1,21 +1,35 @@
 package com.devskiller.dao;
 
 import com.devskiller.model.Item;
+import com.devskiller.model.Review;
+import org.hibernate.SessionFactory;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
-import java.util.ArrayList;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ItemDao extends HibernateDaoSupport {
 
-  public Page<Item> findItems(PageRequest pageRequest) {
-    // TODO: Implement
-    return new Page<>(new ArrayList<>(), 0, 0);
-  }
+    private static final String FIND_ITEM_QUERY = "FROM Item i ORDER BY i.id";
+    private static final String FIND_ITEM_QUERY2 = "FROM Review r RIGHT JOIN r.item WHERE r.rating > ?0";
 
-  public List<Item> findItemsWithAverageRatingLowerThan(Integer rating) {
-    return new ArrayList<>();
-  }
+    @Transactional
+    public Page<Item> findItems(PageRequest pageRequest) {
+        List<Item> items = getHibernateTemplate().find(FIND_ITEM_QUERY)
+                .stream().map(i -> (Item) i)
+                .collect(Collectors.toList());
+        return new Page<>(items, pageRequest.getPageNumber(), pageRequest.getCount());
+    }
 
+    @Transactional
+    public List<Item> findItemsWithAverageRatingLowerThan(Integer rating) {
+        List<Item> items = getHibernateTemplate().find(FIND_ITEM_QUERY2, rating)
+                .stream()
+                .map(r -> (Review) r)
+                .map(r -> r.getItem())
+                .collect(Collectors.toList());
+        return items;
+    }
 
 }
