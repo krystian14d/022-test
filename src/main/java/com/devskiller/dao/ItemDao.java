c8666d482eee43f47a38849b1972f78449f6dd20
Krystian <krystian14d@gmail.com>
2022-08-21 12:14:06 +0200

    solution

--- src/main/java/com/devskiller/dao/ItemDao.java
+++ src/main/java/com/devskiller/dao/ItemDao.java
@@ -1,34 +1,59 @@
 package com.devskiller.dao;
 
 import com.devskiller.model.Item;
-import com.devskiller.model.Review;
-import org.hibernate.SessionFactory;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ItemDao extends HibernateDaoSupport {
 
-    private static final String FIND_ITEM_QUERY = "FROM Item i ORDER BY i.id";
-    private static final String FIND_ITEM_QUERY2 = "FROM Review r RIGHT JOIN r.item WHERE r.rating > ?0";
+    private static final int PAGE_SIZE = 10;
+
+    private static final String FIND_ITEMS_BY_RATING_NATIVE_QUERY = "SELECT * FROM item JOIN item.id = review.id " +
+            "WHERE AVG(CONVERT(review.rating, SQL_DOUBLE)) < :value GROUP BY item";
+    private static final String FIND_ITEMS_QUERY = "SELECT i FROM Item i ORDER BY i.id";
+    private static final String FIND_ITEMS_BY_RATING = "SELECT i FROM Item i JOIN i.reviews r " +
+            "WHERE AVG(CAST(r.rating AS double)) < :rtg GROUP BY i";
+    private static final String FIND_ITEMS_BY_RATING2 = "SELECT i FROM Item i JOIN i.reviews r " +
+            "WHERE AVG(r.rating) < :rtg GROUP BY i";
+
 
     @Transactional
     public Page<Item> findItems(PageRequest pageRequest) {
-        List<Item> items = getHibernateTemplate().find(FIND_ITEM_QUERY)
-                .stream().map(i -> (Item) i)
-                .collect(Collectors.toList());
-        return new Page<>(items, pageRequest.getPageNumber(), pageRequest.getCount());
+
+//        List<Item> items = getHibernateTemplate().find(FIND_ITEMS_QUERY)
+//                .stream()
+//                .map(i -> (Item) i)
+//                .collect(Collectors.toList());
+//
+//        PagedListHolder<Item> page = new PagedListHolder<>(items);
+//        page.setPageSize(PAGE_SIZE); //set page size
+//        page.setPage(pageRequest.getPageNumber());
+//
+//        return new Page<>(page.getPageList(), pageRequest.getPageNumber(), pageRequest.getCount());
+
+        List<Item> resultList = currentSession().createQuery(FIND_ITEMS_QUERY, Item.class)
+                .setMaxResults(PAGE_SIZE)
+                .setFirstResult(PAGE_SIZE * pageRequest.getPageNumber())
+                .getResultList();
+
+        return new Page<>(resultList, pageRequest.getPageNumber(), pageRequest.getCount());
     }
 
     @Transactional
     public List<Item> findItemsWithAverageRatingLowerThan(Integer rating) {
-        List<Item> items = getHibernateTemplate().find(FIND_ITEM_QUERY2, rating)
-                .stream()
-                .map(r -> (Review) r)
-                .map(r -> r.getItem())
-                .collect(Collectors.toList());
+
+        List<Item> items = currentSession().createQuery(FIND_ITEMS_BY_RATING2, Item.class)
+                .setParameter("rtg", rating)
+                .getResultList();
+
+//        List<Item> items = (List<Item>) currentSession().createNativeQuery("SELECT * FROM item " +
+//                        "JOIN item.id = review.id " +
+//                        "WHERE AVG(1.0 * review.rating) < :value GROUP BY item")
+//                .addEntity(Item.class)
+//                .setParameter("value", rating.doubleValue())
+//                .getResultList();
         return items;
     }
 
